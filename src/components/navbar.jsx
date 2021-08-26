dcaa71ee0f8bc64aef7df4297826700af479be40
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-08-26 11:27:14 +0900

    commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Navbar extends Component{
+    render(){
+        return(
+            <React.Fragment>
+                <div>
+                    leeaves
+                </div>
+                <div>
+                    Habit 
+                </div>
+            </React.Fragment>              
+        )
+    }
+}
+export default Navbar
